345c6b8025d452252717e86c6b641cb660a01019
Uraaz Gorimar <63547302+urochurro@users.noreply.github.com>
2021-03-04 13:38:18 +0530

    New listing page
    Added new ryde listing page.

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-// hey guys its me piss off
-// heres our dbms project
 
 const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.get('/', function(req,res) {
     res.sendFile(__dirname+"/homepage.html");
+
+app.get('/viewProfile', function(req, res) {
+  res.render('viewProfile.ejs');
 });
+
+
+app.get('/list', function (req, res) {
+    res.render("listing");
 
 app.listen(8000);
 console.log("Server started successfully!");
