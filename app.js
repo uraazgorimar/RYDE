0ffeed7f603b7269a77ef9cceda0e3e6240c4c97
afreenss <61451040+afreenss@users.noreply.github.com>
2021-03-04 13:31:47 +0530

    signinup

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
+<<<<<<< Updated upstream
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+=======
+app.use(express.static(__dirname + "/public"));
+>>>>>>> Stashed changes
 
-app.get('/', function(req,res) {
-    res.sendFile(__dirname+"/homepage.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/homepage.html");
 });
 
+<<<<<<< Updated upstream
 app.get('/viewProfile', function(req, res) {
   res.render('viewProfile.ejs');
+=======
+app.get("/bookingCar", function (req, res) {
+  res.render("bookingCar");
+});
+
+app.get("/signInUp", function (req, res) {
+  res.render("signInUp.ejs");
+>>>>>>> Stashed changes
 });
 
 app.listen(8000);
